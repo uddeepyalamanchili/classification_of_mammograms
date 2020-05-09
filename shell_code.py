3483c8ec87a1ef0f3e4724d44cb0777c54fcb70e
tinku <tinku@localhost.localdomain>
2020-05-09 19:49:29 +0530

    deleting files

--- shell_code.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-list_files = subprocess.run(["python","preproc_san.py","mdb001c.png"],stdout=subprocess.PIPE).stdout.decode('utf-8')
-s = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-print(list_files.split('\n')[-4])
-print(type(s))
-
--- test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import os
-img_name = 'mdb285.pgm'
-img_path = os.path.join(r'/home/tinku/myfiles/final_project/review_2/NEW (copy)//MICROCALCIFICATIONS ONLY/my_code',img_name)
-print(type(img_path))
-print(img_path)
-cv2.imshow("pre",img_path)
-print('end')
